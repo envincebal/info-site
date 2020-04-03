1d49d50add7cc4a3fb94910d09caaf668db0cd2c
envincebal <vincentyan1@gmail.com>
2020-04-03 02:47:37 -0700

    Added routes.

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const http = require("http");
 const fs = require("fs");
 
-http.createServer((req, res) => {
-  if(req.url === "/"){
-    fs.readFile("./index.html")
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("./index.html", (err, data) => {
+      if (err) {
+        res.write("No file found. Please try again.");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("./about.html", (err, data) => {
+      if (err) {
+        res.write("No file found. Please try again.");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  } else if (req.url === "/contact-me") {
+    fs.readFile("./contact.html", (err, data) => {
+      if (err) {
+        res.write("No file found. Please try again.");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+  } else {
+    fs.readFile("./404.html", (err, data) => {
+      if (err) {
+        res.write("No file found. Please try again.");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
   }
-}).listen(3000);
+})
+
+server.listen(3000);
